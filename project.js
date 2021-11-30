77d59f5aa75d411de6f664123aa85d20ba49432f
xionSM <be-li-ath@hotmail.com>
2021-11-30 19:37:05 +0200

    Play a Game of RPS

---
+++ project.js
@@ -0,0 +1,76 @@
+const Rock = "rock";
+
+const Paper = "paper";
+
+const Scissors = "scissors";
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function computerPlay() {
+    if (getRandomInt(1, 3) == 1) {
+        return Rock
+    } else if (getRandomInt(1, 3) == 2) {
+        return Paper
+    } else {
+        return Scissors
+    }
+}
+
+let playerWins = 0;
+let computerWins = 0;
+let rounds = 0;
+
+function game() {
+    
+    const computerSelection = computerPlay();
+    const playerSelection = prompt("Choose 'Rock', 'Paper' or 'Scissors'").toLowerCase();
+    if (playerSelection !== Rock && playerSelection !== Paper && playerSelection !== Scissors) {
+        console.log("Please input Rock, Paper or Scissors");
+        return;
+    }
+    function playRound(playerSelection, computerSelection) { 
+        
+        if (playerSelection === Rock && computerSelection === "Scissors"){
+            console.log("You Win this round! Rock beats Scissors");
+            return 1
+        }  else if (playerSelection === Paper && computerSelection === "Rock") {
+            console.log("You Win this round! Paper beats Rock");
+            return 1
+        }  else if (playerSelection === Scissors && computerSelection === "Paper") {
+            console.log("You Win this round! Scissors beats Paper");
+            return 1
+        } 
+        
+        if (playerSelection === Paper && computerSelection === "Scissors") {
+            console.log("You Lose this round! Scissors beats Paper");
+            return -1
+        } else if (playerSelection === Scissors && computerSelection === "Rock" ) {
+            console.log("You Lose this round! Rock beats Scissors");
+            return -1
+        } else if (playerSelection === Rock && computerSelection === "Paper") {
+            console.log("You Lose this round! Paper beats Rock");
+            return -1
+        }
+        
+        console.log("You tied this round! Try again");
+        return 0
+    }
+    const roundResult = playRound(playerSelection, computerSelection);
+    switch(roundResult) {
+        case 1:
+            playerWins++;
+            break;
+        case -1:
+            computerWins++; 
+            break;
+    }
+    rounds++
+
+    if (rounds <= 20 && playerWins === 3) {
+        return "You've Won the match, Congratulations!"
+    } else if (rounds <= 20 && computerWins === 3) {
+        return "You've Lost the match, Try again."
+    }
+}
