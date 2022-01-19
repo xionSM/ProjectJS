70ca7dddd8e35ad78318f4d039f52449ee14f085
xionSM <be-li-ath@hotmail.com>
2022-01-19 17:26:02 +0200

    Style RPS Game

--- project.js
+++ project.js
@@ -4,6 +4,37 @@ const Paper = "paper";
 
 const Scissors = "scissors";
 
+
+let playerSelection = '';
+
+const body = document.querySelector("body");
+    
+const container = document.createElement("div");
+body.appendChild(container)
+container.classList.add("container")
+
+const button1 = document.createElement("button");
+button1.textContent = Rock;
+button1.classList.add("btn")
+
+const button2 = document.createElement("button");
+button2.textContent = Paper;
+button2.classList.add("btn")
+
+const button3 = document.createElement("button");
+button3.textContent = Scissors;
+button3.classList.add("btn")
+
+container.appendChild(button1)
+container.appendChild(button2)
+container.appendChild(button3)
+
+const buttons = document.querySelectorAll(".btn");
+console.log(buttons)
+
+buttons.forEach(button => button.addEventListener("click", playRound))
+
+
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
@@ -22,61 +53,55 @@ let playerWins = 0;
 let computerWins = 0;
 let rounds = 0;
 
-function game() {
-    
-    if (rounds <=100 && playerWins === 3) {
-        return "You've already won the match!"
-    } else if (rounds <=100 && computerWins === 3) {
-        return "You've already lost the match! You can try reloading and playing again."
-    }
 
+function playRound(e) { 
     const computerSelection = computerPlay();
-    const playerSelection = prompt("Choose 'Rock', 'Paper' or 'Scissors'").toLowerCase();
-    if (playerSelection !== Rock && playerSelection !== Paper && playerSelection !== Scissors) {
-        console.log("Please input Rock, Paper or Scissors");
-        return;   
-    }
-   
-    function playRound(playerSelection, computerSelection) { 
-        
-        if (playerSelection === Rock && computerSelection === Scissors){
-            console.log("You Win this round! Rock beats Scissors");
-            return 1
-        }  else if (playerSelection === Paper && computerSelection === Rock) {
-            console.log("You Win this round! Paper beats Rock");
-            return 1
-        }  else if (playerSelection === Scissors && computerSelection === Paper) {
-            console.log("You Win this round! Scissors beats Paper");
-            return 1
-        } 
-        
-        if (playerSelection === Paper && computerSelection === Scissors) {
-            console.log("You Lose this round! Scissors beats Paper");
-            return -1
-        } else if (playerSelection === Scissors && computerSelection === Rock ) {
-            console.log("You Lose this round! Rock beats Scissors");
-            return -1
-        } else if (playerSelection === Rock && computerSelection === Paper) {
-            console.log("You Lose this round! Paper beats Rock");
-            return -1
-        }
-        
-        console.log("You tied this round! Try again");
-        return 0
-    }
+    
+    let playerSelection = e.target.textContent;
     rounds++
-    let roundResult = playRound(playerSelection, computerSelection);
-    switch(roundResult) {
-        case 1:
-            playerWins++;
-            break;
-        case -1:
-            computerWins++; 
-            break;
+    if (rounds <= 100 && playerWins === 5) {
+        results.textContent = "You've won the match! Congratulations!"
+        return
+    } else if (rounds <= 100 && computerWins === 5) {
+        results.textContent = "You've lost! Try playing again."
+        return
+    } else {
+        
     }
-    if (rounds <= 100 && playerWins === 3) {
-        return "You've Won the match, Congratulations!"
-    } else if (rounds <= 100 && computerWins === 3) {
-        return "You've Lost the match, Try again."
+    if (playerSelection === Rock && computerSelection === Scissors){
+        results.textContent = "You Win this round! Rock beats Scissors";
+        playerWins++
+    }  else if (playerSelection === Paper && computerSelection === Rock) {
+        results.textContent = "You Win this round! Paper beats Rock";
+        playerWins++
+    }  else if (playerSelection === Scissors && computerSelection === Paper) {
+        results.textContent = "You Win this round! Scissors beats Paper";
+        playerWins++
+    } else if(playerSelection === Paper && computerSelection === Scissors) {
+        results.textContent = "You Lose this round! Scissors beats Paper";
+        computerWins++
+    } else if (playerSelection === Scissors && computerSelection === Rock ) {
+        results.textContent = "You Lose this round! Rock beats Scissors";
+        computerWins++
+    } else if (playerSelection === Rock && computerSelection === Paper) {
+        results.textContent = "You Lose this round! Paper beats Rock";
+        computerWins++
+    } else {
+        results.textContent = "You tied this round! Try again";
     }
+    
+    standings.textContent = `You - ${playerWins} --- Computer Wins - ${computerWins} / Round ${rounds}`
+    
+    
 }
+
+
+let results = document.createElement("div");
+results.classList.add('results');
+body.appendChild(results)
+results.textContent = "";
+
+let standings = document.createElement("div");
+standings.classList.add("standing")
+body.appendChild(standings)
+
